44b71d43b46c83a3f878ce9ab981c28c30a55f22
p-i-r-u-m <igr.case@gmail.com>
2025-04-03 13:05:08 +0300

    fix: updated repo name in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,6 @@ export default defineConfig({
     },
   },
   
-  base: '/', // or simply remove this line (defaults to '/')
+  base: 'frontend', // or simply remove this line (defaults to '/')
 
 })
