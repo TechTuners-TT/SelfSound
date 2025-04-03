044e795b926b4dff1fbab37ee9a26bd02888923e
p-i-r-u-m <igr.case@gmail.com>
2025-04-03 13:10:25 +0300

    fix: Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,6 @@ export default defineConfig({
     },
   },
   
-  base: 'frontend', // or simply remove this line (defaults to '/')
+  base: '/frontend/', // or simply remove this line (defaults to '/')
 
 })
