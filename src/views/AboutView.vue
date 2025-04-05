c630a7a14e26cdc7bc1bc3cd93becf3aa278dcf0
Diana <klimetsdiana21@gmail.com>
2025-04-05 22:29:16 +0300

    fix: correct import paths in AboutView.vue for consistency

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import AboutPage from "@components/About_Components/AboutPage.vue";
-import AboutHeader from "@components/About_Components/AboutHeader.vue";
-import AboutHero from "@components/About_Components/AboutHero.vue";
-import BenefitCard from "@components/About_Components/BenefitCard.vue";
-import AboutBenefits from "@components/About_Components/AboutBenefits.vue";
-import AboutWhyChoose from "@components/About_Components/AboutWhyChoose.vue";
-import AboutFooter from "@components/About_Components/AboutFooter.vue";
+import AboutPage from "@/components/About_Components/AboutPage.vue";
+import AboutHeader from "@/components/About_Components/AboutHeader.vue";
+import AboutHero from "@/components/About_Components/AboutHero.vue";
+import BenefitCard from "@/components/About_Components/BenefitCard.vue";
+import AboutBenefits from "@/components/About_Components/AboutBenefits.vue";
+import AboutWhyChoose from "@/components/About_Components/AboutWhyChoose.vue";
+import AboutFooter from "@/components/About_Components/AboutFooter.vue";
 </script>
 
 <template>
