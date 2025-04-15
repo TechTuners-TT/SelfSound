a36ccfd244e09d36bc200a4951a93dedf54ac403
Diana <klimetsdiana21@gmail.com>
2025-04-15 15:14:34 +0300

    feat(SFS-78): Added svg, vue components

---
+++ src/components/About_Components/AboutGradientBackground.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg
+    viewBox="0 0 1920 3300"
+    fill="none"
+    :width="widthAttr"
+    :height="heightAttr"
+    overflow="visible"
+    class="absolute inset-0 -z-10 w-full h-full"
+    preserveAspectRatio="none"
+  >
+    <rect width="1920" height="3300" fill="url(#paint0_linear_871_96)" />
+    <defs>
+      <linearGradient id="paint0_linear_871_96" x1="960" y1="0" x2="960" y2="3300" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#060310" />
+        <stop offset="0.46" stop-color="#000C9C" />
+        <stop offset="0.59" stop-color="#000C9C" />
+        <stop offset="1" stop-color="#6D01D0" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "AboutBackground",
+
+  props: {
+    width: {
+      type: String,
+      required: false
+    },
+
+    height: {
+      type: String,
+      required: false
+    },
+
+    size: {
+      type: String,
+      required: false
+    }
+  },
+
+  computed: {
+    widthAttr() {
+      if (this.height) return undefined;
+      if (this.size) return this.size;
+      return this.width || "1920px";
+    },
+
+    heightAttr() {
+      if (this.width) return undefined;
+      if (this.size) return this.size;
+      return this.height || "3300px";
+    }
+  }
+};
+</script>
