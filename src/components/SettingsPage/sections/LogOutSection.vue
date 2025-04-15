c12ce21352ceee54c43b914a7cf678aa0dbd02a7
p-i-r-u-m <igr.case@gmail.com>
2025-04-15 16:06:15 +0300

    fix(SFS-133): improved settings page

--- src/components/SettingsPage/sections/LogOutSection.vue
+++ src/components/SettingsPage/sections/LogOutSection.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Log out button -->
   <div class="2xl:m-[40px] xl:m-[30px] m-[20px]">
     <button
       class="2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[16px] font-bold text-white hover:text-[#6D01D0] transition-colors duration-200 inter-font"
@@ -7,6 +8,7 @@
     </button>
   </div>
 
+  <!-- Text: Dangereous zone -->
   <div
     class="2xl:mx-[40px] xl:mx-[30px] mx-[20px] 2xl:mt-[60px] xl:mt-[50px] mt-[40px]"
   >
@@ -17,8 +19,9 @@
     </h1>
   </div>
 
+  <!-- Delete your account button -->
   <div
-    class="2xl:mx-[30px] xl:mx-[20px] mx-[10px] 2xl:mt-[30px] xl:mt-[20px] mt-[10px]"
+    class="2xl:mx-[30px] xl:mx-[20px] mx-[10px] 2xl:my-[30px] xl:my-[20px] my-[10px]"
   >
     <button
       class="relative text-[#F43636] font-bold inter-font 2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[16px] px-[10px] py-[3px] rounded-md bg-[#F43636]/20"
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,42 +3,49 @@
     <!-- Main content -->
     <main class="flex flex-col flex-grow overflow-auto">
       <!-- Nav Bar Top -->
-      <div class="h-[40px]">
+      <!-- <div class="h-[40px]">
         <p
           class="2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[16px] font-bold text-white inter-font"
         >
           NavBar Top - 40px
         </p>
         <div class="relative h-full">
-          <!-- Декоративна лінія -->
+          !-- Декоративна лінія --
           <hr
             class="absolute bottom-0 left-0 w-full border-t-2 border-red-500"
           />
         </div>
       </div>
-
+      -->
       <!-- MAIN CONTENT -->
 
+      <!-- Nav Bar -->
+      <div class="h-[80px]">
+        <NavBar />
+      </div>
+
+      <!-- Settings Content -->
       <div class="flex-grow">
         <SettingsCont />
       </div>
 
       <!-- Nav Bar Bottom -->
-      <div class="h-[80px]">
+      <!--<div class="h-[80px]">
         <div class="relative h-full">
-          <!-- Декоративна лінія -->
+          !-- Декоративна лінія --
           <hr class="absolute top-0 left-0 w-full border-t-2 border-red-500" />
           <p
             class="2xl:text-[24px] xl:text-[24px] lg:text-[20px] text-[16px] font-bold text-white inter-font"
           >
             NavBar Bottom - 80px
           </p>
-        </div>
-      </div>
+        </div> 
+      </div> -->
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
 import SettingsCont from "../components/SettingsPage/SettingsContainer.vue";
+import NavBar from "../components/Navigation/NavBar.vue";
 </script>
