27bce2666175eb51f5fe10b4756db5a55538d45a
p-i-r-u-m <igr.case@gmail.com>
2025-04-20 08:17:32 +0300

    fix(SFS-139): fix adapatation of black line div

--- src/components/SettingsPage/SettingsContainer.vue
+++ src/components/SettingsPage/SettingsContainer.vue
@@ -2,8 +2,9 @@
   <!-- Виключаємо min-h-screen, використовуємо h-full, щоб зайняв доступну висоту -->
   <div class="relative flex items-start justify-center w-full h-full">
     <!-- Забираємо h-full / max-h-screen із внутрішнього контейнера або замінюємо їх на max-h-full -->
+
     <div
-      class="relative flex w-full sm:w-1/2 md:w-1/3 bg-black/30 flex flex-col max-h-full h-screen"
+      class="relative flex w-full sm:w-1/2 md:w-1/3 bg-black/30 flex flex-col min-h-screen overflow-y-auto"
     >
       <!-- Section 1 -->
       <section>
