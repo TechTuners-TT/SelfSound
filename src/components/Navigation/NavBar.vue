adc2c02eddf3a4ac85a57522821631c83e39f879
Yura <urijkisil2007@gmail.com>
2025-04-21 20:15:39 +0300

    feat(SFS-141):Add SVG icons for navigation bar: search, home, add post, notifications, profile, and settings

--- src/components/Navigation/NavBar.vue
+++ src/components/Navigation/NavBar.vue
@@ -4,7 +4,7 @@
     <span class="hidden max-md:block">
       <nav
         class="fixed top-0 left-0 w-full h-[80px] px-2 flex justify-between items-center bg-[#060310] z-30" style="background-color: rgba(6, 3, 16, 1)"
-      >
+        @click="closeAddPost" >
         <!-- Centered Header containing the 'S' -->
         <header
           class="text-[34px] w-[34px] h-[34px] text-center text-white capitalize w-full flex justify-center items-center"
@@ -30,7 +30,7 @@
     <!-- Main navigation - sidebar on desktop, bottom bar on mobile -->
     <nav
       class="fixed left-0 h-screen [@media(min-width:1537px)]:w-[100px] [@media(min-width:1537px)]:px-6.25 [@media(min-width:1537px)]:py-7.75 xl:w-[70px] xl:px-3.5 xl:py-5 lg:w-[60px] lg:px-3 lg:py-4 flex justify-center max-md:w-full max-md:h-auto md:w-[60px] max-md:bottom-0 max-md:left-0 md:px-3 md:py-3 md:py-3 max-md:z-50 [@media(max-width:767.5px)]:h-[80px]"
-      style="background-color: rgba(6, 3, 16, 1)" >
+      style="background-color: rgba(6, 3, 16, 1)"  >
       <div
         class="flex flex-col justify-between items-center h-full w-[50px] min-h-full max-md:w-full max-md:h-auto max-md:flex-row max-md:items-center max-md:justify-between - sm:w-[0px] md:w-[50px]"
       >
@@ -50,7 +50,9 @@
           <a href="#home">
             <NavIcon :svg="homeIcon" />
           </a>
-          <NavIcon :svg="addPostIcon" />
+          <NavIcon :svg="addPostIcon" @click="openAddPost" />
+          
+
           <NavIcon :svg="notificationsIcon" />
           <a href="#profile">
             <NavIcon :svg="profileIcon" />
@@ -66,10 +68,15 @@
       </div>
     </nav>
   </div>
+
+ <!-- Modal itself -->
+ <NavBarAddPostModal :isOpen="isAddPostOpen" @close="closeAddPost" />
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import NavIcon from "@/components/Navigation/NavIcon.vue";
+import NavBarAddPostModal from "./NavBarAddPostModal.vue";
 import {
   searchIcon,
   homeIcon,
@@ -77,7 +84,18 @@ import {
   notificationsIcon,
   profileIcon,
   settingsIcon,
-} from "@/components/SVG/Authentication/NavBarIcon";
+} from "@/components/SVG/Nav_Bar_Icon/NavBarIcon";
+
+
+const isAddPostOpen = ref(false);
+const openAddPost = () => {
+  isAddPostOpen.value = true;
+};
+const closeAddPost = () => {
+  isAddPostOpen.value = false;
+};
+
+
 </script>
 <style scoped>
 @media (max-width: 1536px) {
